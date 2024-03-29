6682b9e7de0800a45b5d41a73e60597ab099a601
knock-huynh <huynhtransongngoc@gmail.com>
2024-03-29 22:09:15 +0700

    feat(avatar): add upload to storage

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,8 @@
 import { env } from "@/configs";
+import { Database } from "@/types/supabase";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   env.VITE_SUPABASE_URL,
   env.VITE_SUPABASE_ANON_KEY,
 );
